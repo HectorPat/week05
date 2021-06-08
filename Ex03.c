03e0f47065f95c71b5fbe52573414993abb179a3
HectorPat <83776645+HectorPat@users.noreply.github.com>
2021-06-07 23:07:02 -0500

    Create Ex03.c

---
+++ Ex03.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void){
+  float x1, x2;
+  printf("Asigna un valor para x\n");
+  scanf("%f", &x1);
+  x2= (x1*4/1)-(4/3)+(4/5)-(4/7)+(4/9)-(4/11)+(4/13)-(4/15)+(4/17)-(4/19);
+printf("El resultado = %.2f", x2);
+}
